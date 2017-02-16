f781c6de8f349e9afa9d878c93b23cdfdcf76cf8
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-16 18:28:58 +0000

    create my player class, override chooseMove

---
+++ ai-cwk-1/RobinPlayer160719011.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+class RobinPlayer160719011 extends GomokuPlayer {
+
+	public Move chooseMove(Color[][] board, Color me) {
+	//first arg is boardstate
+	//second arg is color I am playing
+
+	} //chooseMove()
+}//class RobinPlayer160719011
