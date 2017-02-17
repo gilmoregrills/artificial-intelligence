d2df70dc0f40be5df256e1700d1c05a03fd9c926
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-17 07:27:57 +0000

    start working on player class, mostly in pseudo

--- ai-cwk-1/RobinPlayer160719011.java
+++ ai-cwk-1/RobinPlayer160719011.java
@@ -1,10 +1,18 @@
 import java.awt.Color;
+import java.util.Collection;
 
 class RobinPlayer160719011 extends GomokuPlayer {
 
 	public Move chooseMove(Color[][] board, Color me) {
 	//first arg is boardstate
 	//second arg is color I am playing
-
+		
+		while (true) {
+			int row = (int) (Math.random() *8);
+			int col = (int) (Math.random() *8);
+			if (board[row][col] == null) {
+				return new Move(row, col);
+			}	
+		}
 	} //chooseMove()
 }//class RobinPlayer160719011
