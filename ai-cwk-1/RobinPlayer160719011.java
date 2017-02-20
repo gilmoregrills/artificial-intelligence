f39f13f8c7561516ec69d89f5f74d1beefe2a078
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-20 15:05:19 +0000

    compiles, no eval function, not returning move (just scores)

--- ai-cwk-1/RobinPlayer160719011.java
+++ ai-cwk-1/RobinPlayer160719011.java
@@ -1,57 +1,63 @@
 import java.awt.Color;
 import java.util.*;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 class RobinPlayer160719011 extends GomokuPlayer {
 
 	public Move chooseMove(Color[][] board, Color me) {
 	//first arg is boardstate
 	//second arg is color I am playing
-		boolean max = true;
-			
+		//so if I'm black (black goes first) I am max, if not I am min
+		if (me == Color.black) {
+			alphaBeta(board, me, 5, -100, 100, true);
+		} else {
+			alphaBeta(board, me, 5, -100, 100, false);
+		}
+		//return move??
 	}//chooseMove()
 
-	public ArrayList prepareMoves(Color[][] board) {
+	public ArrayList<Move> prepareMoves(Color[][] board) {
 		ArrayList<Move> moves = new ArrayList<Move>(); //needs to be arraylist!!
 		for (int col = 0; col < 8; col++) {
 			for (int row = 0; row < 8; row++) {
 				if (board[row][col] == null) {
-					board[row][col] = new Move(row, col);
-					moves.add(//the Move object
+					moves.add(new Move(row, col));
 				}
 			}
 		}
 		return moves;
-	}
+	}//prepareMoves
 
 	
-	public Move alphaBeta(Color[][] board, Color me, int depth, int alpha, int beta, boolean max) {
-		ArrayList<Move> moves = prepareMoves(board);
-		if depth = 0 || board == terminal??) {
-			//return the score for the terminal state! basically stop the search and return
-			//every move up the call stack!!!
+	public int alphaBeta(Color[][] board, Color me, int depth, int alpha, int beta, boolean max) {
+		ArrayList<Move> moves = prepareMoves(board);//calls prepareMoves to get legal moves
+		if (depth == 0 || board == terminal) {
+			//return the score for the terminal state! basically tells the function to stop
+			//the search and return each move up the call stack
 			//this is either at the win-state or at the final user-defined layer if depth-bounded
 		}
 		if (max) {
-			score = 0;
-			for (legalMove in moves) {
-				//apply move to newBoard
-				score = Math.max(score, alphaBeta(newBoard, me, depth-1, alpha, beta, false));
+			int score = -100;
+			for (Move move : moves) {
+				board[move.row][move.col] = me; //create the subnode board
+				score = Math.max(score, alphaBeta(board, me, depth-1, alpha, beta, false));
 				alpha = Math.max(alpha, score);
+				board[move.row][move.col] = null;
 				if (beta <= alpha) {
-					//break/cut off in some way
+					return beta;//or the other way around?
 				}
 			}
 			return score; //or some variant???	
 		}
 		else {
-			score = 0;
-			for (legalMove in moves) {
-				//apply move to newBoard
-				score = Math.max(score, alphaBeta(newBoard, depth-1, alpha, beta, true));
+			int score = 100;
+			for (Move move : moves) {
+				board[move.row][move.col] = me;
+				score = Math.max(score, alphaBeta(board, me, depth-1, alpha, beta, true));
 				beta = Math.min(beta, score);
+				board[move.row][move.col] = null;
 				if (beta <= alpha) {
-					//break or cut off in some way
+					return alpha;//or the other way around?
 				}
 			}
 			return score; //or some variant???
@@ -59,86 +65,14 @@ class RobinPlayer160719011 extends GomokuPlayer {
 	}//alphaBeta()	
 			
 
-	public int[][] eval(Color[][] board, Color me, Color them) {
+	public int[][] eval(Color[][] board, Color me) {
 
-		
-		//I THINK THIS EVAL FUNCTION IS COMPLETELY WRONG. I'M NOT EVALUATING THE UTILITY OF THE MOVE
-		//I'M INSTEAD EVALUATING THE UTILITY OF THE WHOLE BOARD??
-		//SHOULD CHECK FOR THE PRESENCE OF ROWS OF THE PLAYER COLOUR WHETHER MAX OR MIN
-		//MORE POTENTIAL ROWS THE HIGHER THE UTILITY? FITS WITH THE THREAT THINGw
-		
-		if (board[col][row] == null) {
-			int patternCounter = 0;
-			int tmpRow = row;
-			int tmpCol = col;
-			//check for patterns iterating to the right, add to moves array if > current val
-			//this should be a function tbh!!!
-			do {
-				tmpRow++;
-				if (board[tmpCol][tmpRow] == me) {
-					patternCounter++;
-				} else if (board[tmpCol][tmpRow] == them) {
-					patternCounter--;
-				}
-			} while (board[tmpCol][tmpRow] == me || board[tmpCol][tmpRow] == them);
-			potentialMove[0] = Math.max(patternCounter, moves[col][row]);
-			//check for patterns iterating up, add to moves array if > current val
-			patternCounter = 0;
-			tmpRow = row;
-			tmpCol = col;
-			do {
-				tmpCol++;
-				if (board[tmpCol][tmpRow] == me) {
-					patternCounter++;
-				} else if (board[tmpCol][tmpRow] == them) {
-					patternCounter--;
-				}
-			} while (board[tmpCol][tmpRow] == me || board[tmpCol][tmpRow] == them);
-			potentialMove[0] = Math.max(patternCounter, moves[col][row]);
-			//check for patterns iterating up and to the right
-			patternCounter = 0;
-			tmpRow = row;
-			tmpCol = col;
-			do {
-				tmpRow++;
-				tmpCol++;
-				if (board[tmpCol][tmpRow] == me) {
-					patternCounter++;
-				} else if (board[tmpCol][tmpRow] == them) {
-					patternCounter--;
-				}
-			} while (board[tmpCol][tmpRow] == me || board[tmpCol][tmpRow] == them);
-			potentialMove[0] = Math.max(patternCounter, moves[col][row]);
-			//check for patterns iterating down and to the right
-			patternCounter = 0;
-			tmpRow = row;
-			tmpCol = col;
-			do {
-				tmpRow--;
-				tmpCol--;
-				if (board[tmpCol][tmpRow] == me) {
-					patternCounter++;
-				} else if (board[tmpCol][tmpRow] == them) {
-					patternCounter--;
-				}
-			} while (board[tmpCol][tmpRow] == me || board[tmpCol][tmpRow] == them);
-			potentialMove[0] = Math.max(patternCounter, moves[col][row]);
-			//check for patterns down
-			patternCounter = 0;
-			tmpRow = row;
-			tmpCol = col;
-			do {
-				tmpCol--;
-				if (board[tmpCol][tmpRow] == me) {
-					patternCounter++;
-				} else if (board[tmpCol][tmpRow] == them) {
-					patternCounter--;
-				}
-			} while (board[tmpCol][tmpRow] == me || board[tmpCol][tmpRow] == them);
-			potentialMove[0] = Math.max(patternCounter, moves[col][row]);
-		} else if (board[col][row] != null) {
-			potentialMove[0] = 0;
-		}
-		return potentialMove; //or return the BEST of moves??
+	//I THINK THIS EVAL FUNCTION IS COMPLETELY WRONG. I'M NOT EVALUATING THE UTILITY OF THE MOVE
+	//I'M INSTEAD EVALUATING THE UTILITY OF THE WHOLE BOARD??
+	//SHOULD CHECK FOR THE PRESENCE OF ROWS OF THE PLAYER COLOUR WHETHER MAX OR MIN
+	//MORE POTENTIAL ROWS THE HIGHER THE UTILITY? FITS WITH THE THREAT THINGw	
+	//square contains piece of your colour, check all around it for patterns? then add to main scoring variable
+	//is there a way to check squares off? once they've been included in a pattern they're not to be checked again
+	//depending on what the patternCounter reaches (ie, how long your line is) you pass back a higher score multiplier??
 	}//eval()
 }//class RobinPlayer160719011
