35ce6ed1b39a5e33ec2b32ff4aae4297b15578ac
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-21 23:00:13 +0000

    clear up code, start eval function, currently playing in sequence

--- ai-cwk-1/RobinPlayer160719011.java
+++ ai-cwk-1/RobinPlayer160719011.java
@@ -29,10 +29,10 @@ class RobinPlayer160719011 extends GomokuPlayer {
 
 	public ArrayList<MoveScore> prepareMoves(Color[][] board) {
 		ArrayList<MoveScore> moves = new ArrayList<MoveScore>(); //needs to be arraylist!!
-		for (int col = 0; col < 8; col++) {
-			for (int row = 0; row < 8; row++) {
+		for (int row = 0; row < 8; row++) {
+			for (int col = 0; col < 8; col++) {
 				if (board[row][col] == null) {
-					moves.add(new MoveScore(new Move(row, col), 0)); //maybe null	
+					moves.add(new MoveScore(new Move(row, col), 0)); //maybe null - 22:56	
 				}
 			}
 		}
@@ -42,31 +42,28 @@ class RobinPlayer160719011 extends GomokuPlayer {
 	
 	public MoveScore alphaBeta(Color[][] board, Color me, int depth, int alpha, int beta, boolean max) {
 		if (depth <= 0) {//also needs to be if the board has hit a win condition!!!
-			MoveScore terminalState = new MoveScore(new Move(4, 4), 0); //returns move from bottom of stack, why?
+			MoveScore terminalState = new MoveScore(new Move(4, 4), 0); 
 			terminalState.score = eval(board, me, max);
 			return terminalState;
 
-			//return the score for the terminal state! basically tells the function to stop
-			//the search and return each move up the call stack
-			//this is either at the win-state or at the final user-defined layer if depth-bounded
+			//return the score for the terminal state! Either at win state or once depth has hit 0
 		}
 		else if (max == true) {
-			//21:23 20th Feb - it always tries to go in 4,4 - work out which one is the root problem
-			ArrayList<MoveScore> moves = prepareMoves(board);//calls prepareMoves to get legal moves
+			ArrayList<MoveScore> moves = prepareMoves(board);
 			MoveScore returnedMove;
-			MoveScore bestMove = new MoveScore(new Move(0, 0), -200);
+			MoveScore bestMove = new MoveScore(new Move(4, 4), -200);
 			for (MoveScore currentMove : moves) {
 				board[currentMove.move.row][currentMove.move.col] = me; //create the subnode board
 				//call alphaBeta on subnode board, store score + move in returnedMove
 				returnedMove = alphaBeta(board, me, depth-1, alpha, beta, false);
-				board[currentMove.move.row][currentMove.move.col] = null;//reset the board -- 21:37 might be a problem with the move I return with terminalState
+				board[currentMove.move.row][currentMove.move.col] = null;//reset the board
 				//if returnedMove comes w/ higher score - reduce to ternary op??
-				if (returnedMove.score > bestMove.score || bestMove == null) {
+				if (returnedMove.score > bestMove.score || bestMove == null) {//possibly should just be bestMove = null
 					bestMove.score = returnedMove.score;
 					bestMove.move = currentMove.move;
 				}
 				//if returnedMove has higher score than alpha - reduce to ternary op?
-				if (bestMove.score >= alpha) {
+				if (returnedMove.score >= alpha) {
 					alpha = returnedMove.score;
 				}
 				//pruning - still not sure about this breakoff
@@ -79,21 +76,21 @@ class RobinPlayer160719011 extends GomokuPlayer {
 			return bestMove;
 		}
 		else {
-			ArrayList<MoveScore> moves = prepareMoves(board);//calls prepareMoves to get legal moves
+			ArrayList<MoveScore> moves = prepareMoves(board);
 			MoveScore returnedMove;
 			MoveScore bestMove = new MoveScore(new Move(0, 0), 200);
 			for (MoveScore currentMove : moves) {
 				board[currentMove.move.row][currentMove.move.col] = me;//create the subnode board
 				//call alphabeta on subnode, storing data in returnedMove
 				returnedMove = alphaBeta(board, me, depth-1, alpha, beta, true);
-				board[currentMove.move.row][currentMove.move.col] = null;//undo the move -- 21:39 I think both of these should be board[returnedMove.move.row] etc
+				board[currentMove.move.row][currentMove.move.col] = null;
 				//if returnedMove comes w/ lower score - reduce to ternary op??
-				if (returnedMove.score < bestMove.score || bestMove == null) { //might be something wrong with this evaluation, potentially always sets bestMove to returnedMove
+				if (returnedMove.score < bestMove.score || bestMove == null) {
 					bestMove.score = returnedMove.score;
 					bestMove.move = currentMove.move;
 				}
 				//if returnedMove has lower score than beta - reduce to ternary op
-				if (bestMove.score <= beta) {
+				if (returnedMove.score <= beta) {
 					beta = returnedMove.score;
 				}
 				//pruning - still not sure about this breakoff
@@ -109,13 +106,49 @@ class RobinPlayer160719011 extends GomokuPlayer {
 			
 
 	public int eval(Color[][] board, Color me, boolean max) {
+		/*
 		//placeholder function, assigns random score +50 to -50		
 		Random random = new Random();
 		int randomReturn = (random.nextInt(100)-100);
 		return randomReturn;
-
-	//square contains piece of your colour, check all around it for patterns? then add to main scoring variable
-	//is there a way to check squares off? once they've been included in a pattern they're not to be checked again
-	//depending on what the patternCounter reaches (ie, how long your line is) you pass back a higher score multiplier??
+		*/
+		
+		
+		//22:02 bugfixing - for loops are functional
+	        // to start with I'm going to check each occupied square for patterns up, down, upright, downright, and right from it		
+		int totalScore = 0;
+		int squareCounter = 0;
+		for (int i = 0; i < 8; i++) {//rows
+			for (int j = 0; j < 8; j++) {//cols
+				/*
+				int row;
+				int col;
+				int patternCounter = 0;
+				if (board[i][j] == Color.white) {
+					row = i;
+					col = j;
+					do {
+						col++;
+						patternCounter += 10;
+					} while (board[row][col] == Color.white);
+					totalScore += patternCounter;
+				} else if (board[i][j] == Color.black) {
+					row = i;
+					col = j;
+					do {
+						col++;
+						patternCounter += 10;
+					}while (board[row][col] == Color.black);
+					totalScore += patternCounter;
+				}
+				*/
+				if (board[4][4] == Color.black) {
+					totalScore = 40;
+				} else if (board[5][5] == Color.black) {
+					totalScore = 40;
+				}
+			}
+		}
+		return totalScore;
 	}//eval()
 }//class RobinPlayer160719011
