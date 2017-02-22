37e98b6dc01b0530a042a8ff2a79f13ae541027e
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-22 20:50:22 +0000

    fix getPatterns, add scorePatterns, first commit to this branch

--- ai-cwk-1/RobinPlayer160719011.java
+++ ai-cwk-1/RobinPlayer160719011.java
@@ -43,7 +43,7 @@ class RobinPlayer160719011 extends GomokuPlayer {
 	public MoveScore alphaBeta(Color[][] board, Color me, int depth, int alpha, int beta, boolean max) {
 		if (depth <= 0) {//also needs to be if the board has hit a win condition!!!
 			MoveScore terminalState = new MoveScore(new Move(4, 4), 0); 
-			terminalState.score = eval(board, me, max);
+			terminalState.score = eval(board, max);
 			return terminalState;
 
 			//return the score for the terminal state! Either at win state or once depth has hit 0
@@ -109,24 +109,24 @@ class RobinPlayer160719011 extends GomokuPlayer {
 	}//alphaBeta()	
 			
 
-	public int eval(Color[][] board, Color me, boolean max) {		
-		//22:02 bugfixing - for loops are functional
-	        // to start with I'm going to check each occupied square for patterns up, down, upright, downright, and right from it
-		int totalScore = 0;
-		int squareCounter = 0;
+	public int eval(Color[][] board, boolean max) {		
+		
+		int boardScore = 0;
+		int squareScore = 0;
 		for (int i = 0; i < 8; i++) {//rows
 			for (int j = 0; j < 8; j++) {//cols
 				if (board[i][j] == Color.white) {
 					System.out.println("square: "+i+","+j+"  is white");
-					totalScore += search(board, Color.white, i, j);
+					squareScore = scorePatterns(getPatterns(board, Color.white, i, j));
 				} else if (board[i][j] == Color.black) {
 					System.out.println("square: "+i+","+j+" is black!");
-					totalScore -= search(board, Color.black, i, j);
-				} else if (board[i][j] == null) {
+					squareScore = scorePatterns(getPatterns(board, Color.black, i, j));
+				} else {
 					System.out.println("square is null");
 				}
+				boardScore += squareScore;
 		
-				/*This is my debug code, essentially it's random, 
+				/*This is debug code, essentially it's random, 
 				 * but it tests pruning etc too.
 				 *
 				Random random = new Random();
@@ -139,102 +139,93 @@ class RobinPlayer160719011 extends GomokuPlayer {
 				*/
 			}
 		}
-		System.out.println("Total score for this board is: "+totalScore);
-		return totalScore;
+		System.out.println("Total score for this board is: "+boardScore);
+		return boardScore;
 	}//eval()
-	public int search(Color[][] board, Color me, int row, int col) {
+	public int scorePatterns(ArrayList<Integer> patterns) {
+		int score = 0;
+		return score;
+	}
+	public ArrayList<Integer> getPatterns(Color[][] board, Color me, int row, int col) {
 		 
-			int total = 0;
+			ArrayList<Integer> patterns = new ArrayList<Integer>();
 			int pattern = 0;
 			int counter = 0;
 			int tmpRow = row;
 			int tmpCol = col;
-			for (int i = 0; i < 5; i = pattern) {
-				System.out.println("this is loop: "+i+" we're searching: "+tmpRow+","+tmpCol+" for pattern: "+pattern+" counter: "+counter);
+			for (int i = 1; i < 6; i++) {
+				System.out.println("This loop we're searching pattern: "+i);
+				pattern = i;
+				counter = 1; //could be 0
+				tmpRow = row;
+				tmpCol = col;
 
-				if (board[tmpRow][tmpCol] == me) {
-					counter++;
-					System.out.println("color found: "+board[tmpRow][tmpCol]);
-				} else {
-					pattern++;
-					total += counter * counter;
-					counter = 0;	
-					System.out.println("color found: "+board[tmpRow][tmpCol]);
-					tmpRow = row;
-					tmpCol = col;
-				}
 				switch(pattern) {
-					case 0:
-						if (tmpRow > 0) {
-							tmpRow--;
-							System.out.println("we can search upwards! tmpRow is now: "+tmpRow);
-						} else {
-							pattern++;
-							total += counter * counter;
-							counter = 0;	
-							System.out.println("no searching upwards, now we check pattern "+pattern);
-							tmpRow = row;
+					case 1:		
+						for (int j = 1; j < 6; j++) {
+							if (tmpRow > 0 && board[tmpRow-1][tmpCol] == me) {
+								tmpRow--;
+								System.out.println("Searching upwards");
+								counter++;
+							} else {
+								patterns.add(counter);
+								break;
+							}
 						}
-						break;
-					case 1:
-						if (tmpRow > 0 && tmpCol < 7) {
-							tmpRow--;
-							tmpCol++;
-							System.out.println("we can search up/right! tmpRow is now: "+tmpRow+" tmpCol is now"+tmpCol);
-						} else {
-							pattern++;
-							total += counter * counter;
-							counter = 0;	
-							tmpRow = row;
-							tmpCol = col;
-							System.out.println("no searching up/right, now we check pattern "+pattern);
-						}
-						break;
+						//might need a "break" at every stage here
 					case 2:
-						if (tmpCol < 7) {
-							tmpCol++;
-							System.out.println("we can search right! tmpCol is now: "+tmpCol);
-						} else {
-							pattern++;
-							total += counter * counter;
-							counter = 0;	
-							tmpCol = col;
-							System.out.println("no searching right, now we check pattern"+pattern);
+						for (int k = 1; k < 6; k++) {
+							if (tmpRow > 0 && tmpCol < 7 && board[tmpRow-1][tmpCol+1] == me) {
+								tmpRow--;
+								tmpCol++;
+								System.out.println("Searching up/right");
+								counter++;
+							} else { 
+								patterns.add(counter);
+								break;
+							}
 						}
-						break;
 					case 3:
-						if (tmpRow < 7 && tmpCol < 7) {
-							tmpCol++;
-							tmpRow++;
-							System.out.println("we can search down/right! tmpRow is :"+tmpRow+" tmpCol is :"+tmpCol);
-						} else {
-							pattern++;
-							total += counter * counter;
-							counter = 0;	
-							tmpCol = col;
-							tmpRow = row;
-							System.out.println("no searching down/right, now we check pattern "+pattern);
+						for (int l = 1; l < 6; l++) {
+							if (tmpCol < 7 && board[tmpRow][tmpCol+1] == me) {
+								tmpCol++;
+								System.out.println("Searching right");
+								counter++;
+							} else {
+								patterns.add(counter);
+								break;
+							}
 						}
-						break;
 					case 4:
-						if (tmpRow < 7) {
-							tmpRow++;
-							System.out.println("we can search down! tmpRow is: "+tmpRow+" tmpCol is: "+tmpCol);
-						} else {
-							pattern++;
-							total += counter * counter;
-							counter = 0;	
-							tmpRow = row;
-							System.out.println("no searching down, and this was the last pattern");
+						for (int m = 1; m < 6; m++) {
+							if (tmpCol < 7 && tmpRow < 7 && board[tmpRow+1][tmpCol+1] == me) {
+								tmpCol++;
+								tmpRow++;
+								System.out.println("Searching down/right");
+								counter++;
+							} else {
+								patterns.add(counter);
+								break;
+							}
 						}
-						break;
+					case 5:
+						for (int n = 1; n < 6; n++) {
+							if (tmpRow < 7 && board[tmpRow+1][tmpCol] == me) {
+								tmpRow++;
+								System.out.println("Searching dooooown");
+								counter++;
+							} else {
+								patterns.add(counter);
+								break;
+							}
+						}
 					default:
-						return total;
+						break;
 				}
 				
 				
 			}
 		System.out.println("we've made it to the return of search()");		
-		return total;
+		return patterns;
 	}//search()
 }//class RobinPlayer160719011
