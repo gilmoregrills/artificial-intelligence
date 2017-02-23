33defd6b4bf8112d4f152a3158838dd4fad8c6c6
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-23 15:46:14 +0000

    clean up code, change comments, make things presentable

--- ai-cwk-1/RobinPlayer160719011.java
+++ ai-cwk-1/RobinPlayer160719011.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 class RobinPlayer160719011 extends GomokuPlayer {
 
+	//the horribly-named MoveScore object holds a move,  and the score returned by eval();
 	private class MoveScore {
 		public Move move;
 		public int score;
@@ -15,24 +16,24 @@ class RobinPlayer160719011 extends GomokuPlayer {
 	}
 
 	public Move chooseMove(Color[][] board, Color me) {
-	//first arg is boardstate
-	//second arg is color I am playing
 		MoveScore myMove;
-		//so if I'm white (white goes first) I am max, if not I am min
+		//white is always max, black is always min
 		if (me == Color.white) {
-			myMove = alphaBeta(board, me, 6, -2000, 2000, true);
+			myMove = alphaBeta(board, me, 4, -2000, 2000, true);
 		} else {
-			myMove = alphaBeta(board, me, 6, -2000, 2000, false);
+			myMove = alphaBeta(board, me, 4, -2000, 2000, false);
 		}
 		return myMove.move;
 	}//chooseMove()
 
 	public ArrayList<MoveScore> prepareMoves(Color[][] board) {
+		//iterates through every index of the board, if there's no piece there it adds 
+		//it as a potential move to the "moves" ArrayList
 		ArrayList<MoveScore> moves = new ArrayList<MoveScore>();
 		for (int row = 0; row < 8; row++) {
 			for (int col = 0; col < 8; col++) {
 				if (board[row][col] == null) {
-					moves.add(new MoveScore(new Move(row, col), 0)); //maybe null - 22:56	
+					moves.add(new MoveScore(new Move(row, col), 0));
 				}
 			}
 		}
@@ -41,14 +42,12 @@ class RobinPlayer160719011 extends GomokuPlayer {
 
 	
 	public MoveScore alphaBeta(Color[][] board, Color me, int depth, int alpha, int beta, boolean max) {
-		if (depth <= 0) {//also needs to be if the board has hit a win condition!!!
+		if (depth <= 0) {//would also be good to return if I hit a win condition, need to test for that 
 			MoveScore terminalState = new MoveScore(new Move(4, 4), 0); 
-			terminalState.score = eval(board, max, me);
+			terminalState.score = eval(board, me);
 			return terminalState;
-
-			//return the score for the terminal state! Either at win state or once depth has hit 0
 		}
-		ArrayList<MoveScore> moves = prepareMoves(board);
+		ArrayList<MoveScore> moves = prepareMoves(board);//prepare the moves
 		if (max == true) {
 			System.out.println("it's max's turn");
 			MoveScore returnedMove;
@@ -93,50 +92,53 @@ class RobinPlayer160719011 extends GomokuPlayer {
 				if (bestMove.score <= beta) {
 					beta = bestMove.score;	
 				}
-				//pruning - still not sure about this breakoff
+				//pruning - still not sure about this but it seems to be working?
 				if (beta <= alpha) {
 					bestMove.score = alpha;
 					bestMove.move = null;
 					return bestMove;
 				}
 			}
-			return bestMove; //or some variant???
+			return bestMove;
 		}
 	}//alphaBeta()	
 			
 
-	public int eval(Color[][] board, boolean max, Color me) {		
-		
+	public int eval(Color[][] board, Color me) {		
+		//this function takes the board, it passes each non-null index to the pattern scoring function
+		//the pattern scoring function is passed the results from the pattern CHECKING function (spaghetti!)
+		//and the resulting score is subtracted or added to the board score
+		//If the patterns are for the opponent, there is a small multiplier added - helps encourage defensive play
 		int boardScore = 0;
 		int squareScore = 0;
 		for (int i = 0; i < 8; i++) {//rows
 			for (int j = 0; j < 8; j++) {//cols
 				if (board[i][j] == Color.white) {
 					squareScore = scorePatterns(getPatterns(board, Color.white, i, j));
-					boardScore += (Color.white == me) ? squareScore : squareScore*2;
+					boardScore += (Color.white == me) ? squareScore : squareScore*1.1;
 				} else if (board[i][j] == Color.black) {
 					squareScore = scorePatterns(getPatterns(board, Color.black, i, j));
-					boardScore -= (Color.black == me) ? squareScore : squareScore*2;
-				} else {
-					
+					boardScore -= (Color.black == me) ? squareScore : squareScore*1.1;
 				}
-				//Whatever player *I* am, I want to give a multiplier to the opponent's
-				//patterns, so that preventing them from developing patterns is > making
-				//my own
 			}
 		}
 		System.out.println("Total score for this board is: "+boardScore);
 		return boardScore;
 	}//eval()
 	public int scorePatterns(ArrayList<Integer> patterns) {
+		//takes list of patterns found from a particular square and multiplies them
+		//so that longer patterns appear more valuable than multiple shorter patterns
 		int score = 0;
 			for (Integer pattern : patterns) {
-				score += (pattern * pattern);
+				score += (pattern * pattern * pattern);
 			}
 		return score;
 	}
 	public ArrayList<Integer> getPatterns(Color[][] board, Color me, int row, int col) {
-		 
+			//not a particularly elegant function (though the patterns are quite nice in solarized
+			//takes a board position, increments row and/or col numbers checking for the same pieces
+			//if found, it increments a counter and adds that counter to the patterns list at the end 
+			//only searches up, down, right and right-side diagonals to save on time
 			ArrayList<Integer> patterns = new ArrayList<Integer>();
 			int pattern = 0;
 			int counter = 0;
@@ -144,7 +146,7 @@ class RobinPlayer160719011 extends GomokuPlayer {
 			int tmpCol = col;
 			for (int i = 1; i < 6; i++) {
 				pattern = i;
-				counter = 0; //could be 0
+				counter = 0;
 				tmpRow = row;
 				tmpCol = col;
 
