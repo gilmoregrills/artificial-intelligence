b5646ea4cf366c7a8ad370a7faa56a274268f6a4
gilmoregrills <robinfarrowyonge@gmail.com>
2017-04-12 16:29:06 +0100

    add simple neural net based on tutorials, tailored to the tutorial problem set

---
+++ Neural-Nets/simpleExample.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def nonlin(x, deriv = False):
+    if(deriv == True):
+        return x * (1 - x)
+    return 1 / (1 + np.exp(-x))
+
+inputs = np.array([  [0, 0],
+                    [0, 1],
+                    [1, 0],
+                    [1, 1]  ])
+
+output = np.array( [[0, 1, 1, 0]]).T
+
+np.random.seed(1)
+
+weights0 = 2*np.random.random((2, 4)) - 1
+weights1 = 2*np.random.random((4, 1)) - 1
+
+print "weights"
+print weights0
+print weights1
+
+for iter in xrange(60000):
+
+    l0 = inputs
+    l1 = nonlin(np.dot(l0, weights0))
+    l2 = nonlin(np.dot(l1, weights1))
+
+    l2_error = output - l2
+
+    l2_delta = l2_error * nonlin(l2, deriv=True)
+
+    l1_error = l2_delta.dot(weights1.T)
+
+    l1_delta = l1_error * nonlin(l1, deriv=True)
+
+    weights1 += l1.T.dot(l2_delta)
+    weights0 += l0.T.dot(l1_delta)
+
+
+print "Output after training:"
+print l2
